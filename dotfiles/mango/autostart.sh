0497724fb2c05e75ad97adf6add06ed67d00366d
huzch <huzch123@gmail.com>
2026-01-09 18:07:52 +0800

    clipboard bug

--- dotfiles/mango/autostart.sh
+++ dotfiles/mango/autostart.sh
@@ -2,5 +2,5 @@
 
 waybar & swaync & swww-daemon &
 fcitx5 --replace -d &
-wl-paste --type text --watch cliphist store
-wl-paste --type image --watch cliphist store
+wl-paste --type text --watch cliphist store &
+wl-paste --type image --watch cliphist store &
