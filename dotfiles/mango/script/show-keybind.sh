9ebb4ddd247514f482a5219b1ca39d77b0b01d40
huzch <huzch123@gmail.com>
2026-01-09 18:01:20 +0800

    show change

--- dotfiles/mango/script/show-keybind.sh
+++ dotfiles/mango/script/show-keybind.sh
@@ -1,31 +1,30 @@
 #!/usr/bin/env bash
 
 keybinds=$(cat <<EOF
-<b>窗口管理</b>
-Alt + Return       打开终端
-Alt + Q            关闭窗口
-Alt + Space        应用启动器
-Alt + T            窗口浮动
-Alt + F            最大化
-Alt + Shift + F    全屏
-<b>窗口焦点</b>
-Alt + H            左
-Alt + J            下
-Alt + K            上
-Alt + L            右
-<b>工作区</b>
-Alt + 1-0          切换工作区
-Alt + Tab          上个工作区
-Alt + Shift + 1-0  移动窗口
-<b>系统</b>
-Alt + M            关机菜单
-Alt + N            通知中心
-Alt + V            剪贴板历史
-Alt + W            壁纸循环
-Alt + Shift + W    选择壁纸
-Alt + /            显示快捷键
-Print              截图窗口
-Shift + Print      截图区域
+Alt + Return               打开终端
+Alt + Q                    关闭窗口
+Alt + Space                应用启动器
+Alt + \                    窗口浮动
+Alt + F                    最大化
+Alt + Shift + F            全屏
+Alt + H                    左
+Alt + J                    下
+Alt + K                    上
+Alt + L                    右
+Alt + Shift + H            向左交换窗口
+Alt + Shift + J            向下交换窗口
+Alt + Shift + K            向上交换窗口
+Alt + Shift + L            向右交换窗口
+Alt + 1-0                  切换工作区
+Alt + Shift + 1-0          移动窗口
+Alt + Tab                  展示所有窗口
+Alt + M                    关机菜单
+Alt + N                    通知中心
+Alt + V                    剪贴板历史
+Alt + W                    壁纸循环
+Alt + Shift + W            选择壁纸
+Alt + /                    显示快捷键
+Super + N                  切换布局
 EOF
 )
 
