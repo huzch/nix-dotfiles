9c83718e419a4337a67c6ab10c954496f2f73008
huzch <huzch123@gmail.com>
2026-01-16 15:02:25 +0800

    swapwindow

--- dotfiles/hypr/script/help.sh
+++ dotfiles/hypr/script/help.sh
@@ -4,7 +4,7 @@ keybinds=$(cat <<EOF
 Alt + Return               打开终端
 Alt + Q                    关闭窗口
 Alt + Space                应用启动器
-Alt + \                    窗口浮动
+Alt + T                    窗口浮动
 Alt + F                    最大化
 Alt + Shift + F            全屏
 Alt + H                    左
@@ -17,7 +17,7 @@ Alt + Shift + K            向上交换窗口
 Alt + Shift + L            向右交换窗口
 Alt + 1-0                  切换工作区
 Alt + Shift + 1-0          移动窗口
-Alt + Tab                  展示所有窗口
+Alt + Tab                  切换最近窗口
 Alt + M                    关机菜单
 Alt + N                    通知中心
 Alt + V                    剪贴板历史
@@ -26,7 +26,6 @@ Alt + Shift + P            区域截图
 Alt + W                    壁纸循环
 Alt + Shift + W            选择壁纸
 Alt + /                    显示快捷键
-Super + N                  切换布局
 EOF
 )
 
